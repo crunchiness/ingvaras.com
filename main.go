9b091499e2e80d02780d510478e15fda8a213402
Ingvaras Merkys <ingvaras@gmail.com>
2019-01-14 19:05:19 +0200

    Added some meta tags and removed APIs

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 func init() {
 	router := httprouter.New()
 	router.GET("/", handler)
-	router.GET("/apis", apisHandler)
 	router.GET("/artwork/:artist/:album", ingvaras.ArtworkHandler)
 	router.GET("/artwork/:artist/:album/:raw", ingvaras.ArtworkHandler)
 	router.GET("/tts/:lang/*query", ingvaras.TtsHandler)
@@ -23,8 +22,4 @@ func handler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/404.htm")
-}
-
-func apisHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	http.ServeFile(w, r, "static/apis.htm")
 }
